1b96f0ade43d615d397b5f0968d29517c64f1052
hophi123 <hoangphi120398@gmail.com>
2020-07-24 22:05:05 +0700

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { TeacherBodyComponent } from './teacher-body/teacher-body.component'
+import { TeacherBodyComponent } from './teacher-body/teacher-body.component';
+import { StudentBodyComponent } from './student-body/student-body.component'
 
 const routes: Routes = [
-  {path: 'teacher', component: TeacherBodyComponent}
+  {path: 'teacher', component: TeacherBodyComponent},
+  {path: 'student', component: StudentBodyComponent}
 ]
 
 @NgModule({
--- src/app/student-body/student-body.component.ts
+++ src/app/student-body/student-body.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../header/data.service'
 
 @Component({
   selector: 'app-student-body',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentBodyComponent implements OnInit {
 
-  constructor() { }
+  khoas: any;
+  lops: any;
+  constructor(private data: DataService) { 
+    this.khoas = this.data.khoas;
+    this.lops = this.data.lops;
+  }
 
   ngOnInit(): void {
   }
